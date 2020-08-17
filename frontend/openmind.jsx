c518f1326bc559694dfbad99efe7cd38e1f9fac0
nykiway <nicoleoliviawiehe@gmail.com>
2020-08-17 15:34:20 -0700

    Minor changes

--- frontend/openmind.jsx
+++ frontend/openmind.jsx
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 
 // Components
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Openmind</h1>, root);
-})
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  const store = configureStore();
+  ReactDOM.render(<h1>OpenMind</h1>, root);
+});
