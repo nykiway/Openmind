ace89a7b3d24864a741e90a16e69f54bde202da3
nykiway <nicoleoliviawiehe@gmail.com>
2020-08-17 13:31:29 -0700

    Complete User Model

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ApplicationRecord
+  validates :password_digest, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  validates :username, presence: true, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.base64(64)
+    self.save!
+    self.session_token
+  end
+
+  private
+  
+  def ensure_session_token
+    self.session_token ||= SecureRandom.base64(64)
+  end
+end
