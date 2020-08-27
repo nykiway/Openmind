cae9d32caa4518be8317b8f4578a74259a587928
nykiway <nicoleoliviawiehe@gmail.com>
2020-08-27 14:16:56 -0600

    Update working timer

--- frontend/components/discover/timers/timers.jsx
+++ frontend/components/discover/timers/timers.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DiscoverNav from '../../profile/sub_navs/discover_nav';
+import moment from 'moment';
 
 class Timers extends React.Component {
   constructor(props) {
@@ -24,15 +25,63 @@ class Timers extends React.Component {
     clearInterval(this.timer)
   }
 
+  resetTimer() {
+    this.setState({count: (this.state.count = 0)});
+  }
+
+  secondsToTime(secs) {
+    let hours = Math.floor(secs / (60 * 60));
+
+    let divisor_for_minutes = secs % (60 * 60);
+    let minutes = Math.floor(divisor_for_minutes / 60);
+
+    let divisor_for_seconds = divisor_for_minutes % 60;
+    let seconds = Math.ceil(divisor_for_seconds);
+
+    let formatted =
+      hours.toString().padStart(2, "0") +
+      ":" +
+      minutes.toString().padStart(2, "0") +
+      ":" +
+      seconds.toString().padStart(2, "0");
+
+    return formatted;
+  }
+
   render() {
     return (
       <div>
         <DiscoverNav />
-        <h1>This is where the timers go!</h1>
-        <h2>{this.state.count}</h2>
-        <div>
-          <button onClick={this.startTimer.bind(this)}>Start</button>
-          <button onClick={this.stopTimer.bind(this)}>Stop</button>
+        <div className="timers-section">
+          <img
+            src="https://openmind-seeds.s3-us-west-1.amazonaws.com/images/hero_images/smiles.jpg"
+            alt="timers hero image"
+          />
+          <h1 className="unlimited-timer">The Unlimited Timer</h1>
+          <div className="count-up-timer">
+            <h2 className="count-number">{this.secondsToTime(this.state.count)}</h2>
+            <button
+              id="count-up-button-1"
+              className="count-up-button"
+              onClick={this.startTimer.bind(this)}
+            >
+              Start
+            </button>
+            <button
+              id="count-up-button-2"
+              className="count-up-button"
+              onClick={this.stopTimer.bind(this)}
+            >
+              Stop
+            </button>
+            <button
+              id="reset-timer-button"
+              className="count-up-button"
+              onClick={this.resetTimer.bind(this)}
+            >
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     );
