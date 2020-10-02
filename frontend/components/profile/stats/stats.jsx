d64fa9104dd62df420be207d156716b6305fc7a0
nykiway <nicoleoliviawiehe@gmail.com>
2020-10-02 11:43:57 -0600

    Convert stats to css grid

--- frontend/components/profile/stats/stats.jsx
+++ frontend/components/profile/stats/stats.jsx
@@ -25,22 +25,22 @@ class Stats extends React.Component {
         </div>
         <br />
         <div className="stats">
-          <div className="total-time-meditated">
+          <div className="stat">
             <i className="fas fa-chart-pie"></i>
             <h2>Total time meditated</h2>
             <h3>Test</h3>
           </div>
-          <div className="sessions-completed">
+          <div className="stat">
             <i className="fas fa-headphones"></i>
             <h2>Sessions Completed</h2>
             <h3>Test</h3>
           </div>
-          <div className="current-run-streak">
+          <div className="stat">
             <i className="fas fa-stopwatch"></i>
             <h2>Current run streak</h2>
             <h3>Test</h3>
           </div>
-          <div className="last-time-meditated">
+          <div className="stat">
             <i className="far fa-clock"></i>
             <h2>Last time meditated</h2>
             <h3>Test</h3>
