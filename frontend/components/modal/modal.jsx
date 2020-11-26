34e516935c36ebab79f09143d55ee0114619afbd
nykiway <nicoleoliviawiehe@gmail.com>
2020-11-26 09:36:05 -0700

    Fix for meditation play! 100% complete

--- frontend/components/modal/modal.jsx
+++ frontend/components/modal/modal.jsx
@@ -1,59 +1,120 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import moment from 'moment';
 import MeditationItemContainer from '../discover/meditation_item_container'
 import { closeModal } from '../../actions/modal_actions';
-import { toggleMeditation, nullCurrentMeditation } from '../../actions/current_meditation_actions';
+import { 
+  toggleMeditation, 
+  nullCurrentMeditation,
+  receiveCurrentMeditation
+  } from '../../actions/current_meditation_actions';
+import { fetchMeditation } from '../../actions/meditation_actions'
 
 
 class Modal extends React.Component {
   constructor(props) {
     super(props);
+    this.audioRef = React.createRef();
     this.state = {
+      playing: false
     }
     this.handleModalClose = this.handleModalClose.bind(this);
+    this.controlAudio = this.controlAudio.bind(this);
+    this.renderDuration = this.renderDuration.bind(this);
   }
 
   handleModalClose() {
+    this.setState({ playing: !this.state.playing});
+    this.audioRef.current.pause();
     this.props.closeModal();
-    this.props.nullCurrentMeditation();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.playing !== this.props.playing) {
+      this.state.playing ? this.audioRef.current.play() : this.audioRef.current.pause();
+    }
+  }
+
+  controlAudio() {
+    this.setState({ playing: !this.state.playing })
+
+    if(this.state.playing) {
+      this.audioRef.current.pause();
+    } else {
+      this.audioRef.current.play();
+    }
+  }
+
+  renderDuration() {
+    const seconds = this.props.currentMeditation.duration;
+    return moment.duration(seconds, "seconds").humanize();
   }
 
   render() {
     let { modal } = this.props;
+    const { currentMeditation } = this.props;
+
+    // modal functionality:
 
     if (!modal) {
       return null;
     }
 
-  let component;
-  switch(modal) {
-    case 'meditation':
-      component = <MeditationItemContainer />;
-      break;
-    default:
-      return null;
-  }
-  
-    return (
-      <div className="modal-background">
-        <div 
-          onClick={this.handleModalClose} 
-          className="close-x">
-          <i className="fas fa-times"></i>
+    let component;
+    switch(modal) {
+      case 'meditation':
+        component = <MeditationItemContainer />;
+        break;
+      default:
+        return null;
+    }
+
+    // dynamic pause/ play buttons
+    
+    const pauseIcon = <i className="fas fa-pause meditation-stack-1x" />
+    const playIcon = <i className="fas fa-play meditation-stack-1x"  />
+
+    // if current meditation exists, return modal : don't
+
+    if (Object.keys(this.props.currentMeditation).length !== 0) { 
+      return (
+        <div className="modal-background">
+          <div 
+            onClick={this.handleModalClose} 
+            className="close-x">
+            <i className="fas fa-times"></i>
+          </div>
+          <div className="modal-child" onClick={(e) => e.stopPropagation()}>
+            <div className="meditation-play">
+              <h1 className="meditation-name-title">{currentMeditation.name}</h1>
+              <h2 className="meditation-length">{`${this.renderDuration()}`}</h2>
+              <button onClick={() => this.controlAudio()}>
+                <div className="transparent-circle" >
+                  <div className="opaque-circle" >
+                    {this.state.playing ? pauseIcon : playIcon}
+                  </div>
+                </div>
+              </button>
+              <h3 id="counter" className="timer-countdown"></h3>
+            </div>
+            <audio ref={this.audioRef} src={this.props.currentMeditation.mp3}></audio>
+          </div>
         </div>
-        <div className="modal-child" onClick={(e) => e.stopPropagation()}>
-          {component}
-        </div>
-      </div>
-    );
+      )
+    } else {
+      return (
+        null
+      )
+    }
   }
 }
 
 const mapStateToProps = state => {
   return {
-    modal: state.ui.modal,
+    meditations: Object.values(state.entities.meditations),
+    currentMeditation: state.ui.currentMeditation,
     isPlaying: state.ui.isPlaying,
-    currentMeditation: state.ui.currentMeditation
+    modal: state.ui.modal,
   };
 };
 
@@ -61,8 +122,9 @@ const mapDispatchToProps = dispatch => {
   return {
     closeModal: () => dispatch(closeModal()),
     toggleMeditation: () => dispatch(toggleMeditation()),
-    nullCurrentMeditation: () => dispatch(nullCurrentMeditation())
-    
+    nullCurrentMeditation: () => dispatch(nullCurrentMeditation()),
+    fetchMeditation: (id) => dispatch(fetchMeditation(id)),
+    receiveCurrentMeditation: (id) => dispatch(receiveCurrentMeditation(id)),
   };
 };
 
